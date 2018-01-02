daffdac3529eba664112cefa4601c089789c60e1
Jason Home Computer <wangshen126@126.com>
2018-01-02 22:38:08 +0800

    首页搜索组件完成

--- src/components/index/searchNav/searchNav.js
+++ src/components/index/searchNav/searchNav.js
@@ -9,12 +9,12 @@ class SearchNav extends Component {
 			<div className="searchNav">
 			    <section className="roll searchStyle">
 			      <section className="left">
-					<img src={require('./../../../assets/index/6.png')} />
+					<img src={require('./../../../assets/index/6.png')} alt="登录状态" />
 			        <p>已登录</p>
 			      </section>
 			      <input type="text" name="search" placeholder="  搜索" />
 			      <section className="right">
-					<img src={require('./../../../assets/index/26.png')} />
+					<img src={require('./../../../assets/index/26.png')} alt="消息状态" />
 			        <p>消息</p>
 			      </section>
 			    </section>
